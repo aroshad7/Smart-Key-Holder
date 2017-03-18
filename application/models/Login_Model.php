1aaac25483a3a9ba75d3b05ba20062f6478c50a7
Arosha Dissanayake <aroshad.14@cse.mrt.ac.lk>
2017-03-18 13:28:47 +0530

    Initial commit with codeignitor.

---
+++ application/models/Login_Model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arosha D
+ * Date: 3/18/2017
+ * Time: 1:21 PM
+ */
---
+++ loginDataPost.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arosha D
+ * Date: 3/18/2017
+ * Time: 12:43 PM
+ */
+
+$username = $_POST["username"];
+$password = $_POST["password"];
